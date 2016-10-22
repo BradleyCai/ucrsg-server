26685419c5770be9956f76ef0b27c1c900e31453
Ammon Smith <ammon.i.smith@gmail.com>
2016-10-22 09:15:03 -0700

    Initial project files.

---
+++ src/main/java/org/libucr/course/server/CourseServerApplication.java
@@ -0,0 +1,12 @@
+package org.libucr.course.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseServerApplication {
+    public static void main(final String[] args) {
+        SpringApplication.run(CourseServerApplication.class, args);
+    }
+}
+
